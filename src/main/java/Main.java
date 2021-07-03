391ce7ffabc8c928f3426628858025acdc22bd7d
Gersho <gersho2304@Gmail.com>
2021-07-03 12:47:09 +0300

    Remove 2 lines

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, My name is Orel");
         System.out.println("Hello World, My name is Neta");
-        System.out.println("Hello World, My name is Roni");
-        System.out.println("Hello World, My name is fgd");
     }
 }
