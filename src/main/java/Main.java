1ebf186e6a228db892ef62f7cf632e5c6210ff68
Gersho <gersho2304@Gmail.com>
2021-07-03 11:44:28 +0300

    add my wife name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, My name is Orel");
+        System.out.println("Hello World, My name is Neta");
     }
 }
