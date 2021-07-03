76b096b218a2170b467f6ba8455b9272492562c6
Gersho <gersho2304@Gmail.com>
2021-07-03 12:55:32 +0300

    Add a roni name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, My name is Orel");
         System.out.println("Hello World, My name is Neta");
+        System.out.println("Hello World, My name is Roni");
     }
 }
